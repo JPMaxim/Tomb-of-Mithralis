de9039da926a52e1efe83a6d25799c74bc0880c6
TomKeny <tomkenyon2003@gmail.com>
2023-11-13 15:24:35 +0000

    added files
    Added:
    -classes.js
    -functions.js
    -inquiries.js
    -imports in index.js

---
+++ index.js
@@ -0,0 +1,3 @@
+import {      /*<-- function names here*/} from "./functions.js"
+import {      /*<-- class names here*/} from "./classes.js"
+import {      /*<-- inquiry funciton names*/} from "./inquiries.js"
