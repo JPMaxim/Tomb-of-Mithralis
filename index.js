6af9133f53ccd54e6964884e75476cb3a2cfb1cd
faztab <faizanfawad00@gmail.com>
2023-11-15 15:46:11 +0000

    dialogue test

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 import { combat,heal,hunkerDown,strongBlow,chargeAttack,coinToss,Puzzle    /*<-- function names here*/} from "./functions.js"
 import { Enemy,Player      /*<-- class names here*/} from "./classes.js"
 import {difficultyInquiry, classInquiry , nameInquiry, wait      /*<-- inquiry funciton names*/} from "./inquiries.js"
-import {combatOneHint, combatTwoHint, combatThreeHint      /*<-- dialogue variable names*/} from "./dialogue.js"
+import {combatOneHint, combatTwoHint, combatThreeHint,      /*<-- dialogue variable names*/
+elfIntroScene,
+dwarfIntroScene,
+humanIntroScene,
+wizardIntroScene} from "./dialogue.js"
 
 // run difficulty inquiry
 let difficulty = await difficultyInquiry();
@@ -31,13 +35,22 @@ let choice = characterClass.class;
 let player
 
 if ( choice =="Elf - (Health Specialist)") {player = new Player(125,15,15,heal,"Heal", characterName.name);
-await wait ("Elf dialogue before first fight");}
+await wait ("Elf dialogue for intro scene");
+console.log(elfIntroScene)}
  else if (choice == "Dwarf - (Defense Specialist)" ) {player = new Player(100,15,25,hunkerDown,"Hunker Down", characterName.name);
-await wait ("Dwarf dialogue before first fight");}
+await wait ("Dwarf dialogue for intro scene");
+console.log(dwarfIntroScene)}
   else if (choice == "Human - (Attack Specialist)") {player = new Player(100,20,15,strongBlow,"Strong Blow", characterName.name);
-await wait ("Human dialogue before first fight");}
+await wait ("Human dialogue for intro scene ");
+console.log(humanIntroScene)}
    else if (choice == "Wizard - (Luck Specialist)") {player = new Player(100,10,16,coinToss,"Coin Toss", characterName.name); 
-await wait ("wizard dialogue before first fight");}
+await wait ("wizard dialogue for intro scene");
+console.log(wizardIntroScene)}
+
+if ( choice =="Elf - (Health Specialist)") {await wait ("Elf dialogue before first fight");}
+ else if (choice == "Dwarf - (Defense Specialist)" ) {await wait ("Dwarf dialogue before first fight");}
+  else if (choice == "Human - (Attack Specialist)") {await wait ("Human dialogue before first fight");}
+   else if (choice == "Wizard - (Luck Specialist)") {await wait ("wizard dialogue before first fight");}
 
 // Combat 1 Hint
 if (difficulty.difficulty == "Easy - (hints included)") {
